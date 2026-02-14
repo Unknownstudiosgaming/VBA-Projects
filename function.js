00f850b6f76044fb914ab62527085e38c60bba33
UnknownStudiosG <renierzachary@outlook.com>
2026-02-14 11:13:24 -0500

    Buttons!

--- function.js
+++ function.js
@@ -1,5 +1,12 @@
-window.prompt("Do you hate this pop up?");
-
+window.onload = function() {
 let test = "Page inpsector";
 
 console.log("You earned the badge called: " + test);
+
+let test1 = document.getElementsByClassName("Sbtn");
+
+for (let button of test1) {
+
+button.onclick = function() {
+    window.alert("The engine has opened in another tab, thank you for using this Hub!")
+}}}
