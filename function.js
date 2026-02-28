46030e91784793846670aeb1f8e420304c133233
UnknownStudiosG <renierzachary@outlook.com>
2026-02-27 20:37:16 -0500

    BIG GIANT UPDATE

--- function.js
+++ function.js
@@ -2,11 +2,13 @@ window.onload = function() {
 let test = "Page inpsector";
 
 console.log("You earned the badge called: " + test);
+}
 
-let test1 = document.getElementsByClassName("Sbtn");
+let test1 = document.getElementsByClassName("SEB");
 
 for (let button of test1) {
 
 button.onclick = function() {
-    window.alert("The engine has opened in another tab, thank you for using this Hub!")
-}}}
+    window.alert("The Search Engine you chose has opened in another tab, Thank you for using this Project!");
+}}
+
